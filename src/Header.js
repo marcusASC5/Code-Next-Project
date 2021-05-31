7b33532c7e3b5d0efec108138ee92797af0fba0f
marcusASC5 <marcus.sutton29@yahoo.com>
2021-05-31 14:27:35 -0400

    2 files modified

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import {img} from "bkchess.png";
 
 export const Header = () => {
   return <header>
     <div>Hello, welcome to Bkchess!</div>
     <div id="bg"></div>
-    <div id="logo"></div>
+    <img src="./bkchess.png" class="logo"/>
   </header>
   
 }
