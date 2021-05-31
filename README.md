445bfb596906ec8217efada25893ec5e55b4eae5
marcusASC5 <marcus.sutton29@yahoo.com>
2021-05-31 14:11:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qpu8my
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qpu8my)
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return <header>
+    <div>Hello, welcome to Bkchess!</div>
+    <div id="bg"></div>
+    <div id="logo"></div>
+  </header>
+  
+}
